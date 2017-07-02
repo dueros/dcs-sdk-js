bb27bdc82e6b094235338244276ca93b07cb6de7
wp@rmbp <wwwppp0801@qq.com>
2017-07-03 00:41:09 +0800

    fix protocol

--- dcs_protocol.js
+++ dcs_protocol.js
@@ -46,30 +46,12 @@ function createEvent(namespace,name, context,_payload){
 function createRecognizeEvent(options){
     var ev={
         "requestId":"wp"+new Date().getTime(),
-        "deviceInterface": [
-            {
-                "header": {
-                    "namespace": "ai.dueros.device_interface.audio_player",
-                    "name": "AudioPlayerInterface",
-                },
-                "payload": {}
-            },
-        ],
-        "clientContext": [
-            {
-                "header": {
-                    "namespace": "ai.dueros.context.audio_player",
-                    "name": "PlaybackState",
-                },
-                "payload": {}
-            },
-        ],
         "event" : {
             "header" : {
                 "namespace" : "ai.dueros.device_interface.voice_input",
                 "name" : "ListenStarted",
-                "messageId" : "71c0cf96-6243-4fff-853d-7d63ef4123dd",
-                "dialogRequestId" : "e5c713d0-f5ec-48c6-89bf-a023c38512d7"
+                "messageId" : "wp"+new Date().getTime(),
+                "dialogRequestId" : "wp"+new Date().getTime()
             },
             "payload" : {
                 "format" : "AUDIO_L16_RATE_16000_CHANNELS_1"
