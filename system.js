4c5ea3e7e8899ca8ee07ff4ae28c5dd98cd4cdd9
wp@rmbp <wwwppp0801@qq.com>
2017-08-02 19:32:41 +0800

    add system

---
+++ system.js
@@ -0,0 +1,15 @@
+const child_process=require("child_process");
+
+var unameAll=child_process.execSync("uname -a").toString();
+
+function get(){
+    if(unameAll.match(/raspberrypi/)){
+        return "raspberrypi";
+    }
+    if(unameAll.match(/Darwin/)){
+        return "macos";
+    }
+    return "unknown";
+}
+
+module.exports.get=get;
