0f66394e16cd1e156c01ebc9141762ea8d708de1
wwwppp0801 <wwwppp0801@gmail.com>
2018-02-13 16:56:56 +0800

    remove yuyin wakeup

--- wakeup/new_wakeup/kill.sh
+++
@@ -1,2 +0,0 @@
-ps aux|grep WAKEUP_STANDALONE |grep -v grep|awk '{print $2}'|xargs kill -9
-ps aux|grep rec |grep -v grep|awk '{print $2}'|xargs kill -9
--- wakeup/new_wakeup/run.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-cd $(dirname $0)
-AUDIODEV=hw:1,0 rec -t s16 -c1 -r16000 -b16 - 2>/dev/null|./WAKEUP_STANDALONE 2>/dev/null
