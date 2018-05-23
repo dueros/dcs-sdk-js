c43bf291dc85bfdc8fb89f5a384c04797bc694a5
wwwppp0801 <wwwppp0801@gmail.com>
2018-05-23 11:34:38 +0800

    fix

--- event_test.sh
+++ event_test.sh
@@ -28,20 +28,12 @@ Content-Type: application/json; charset=UTF-8
 {
   "event": {
     "header": {
-      "namespace": "ai.dueros.device_interface.extensions.replay",
-      "name": "ReplayRequested",
+      "namespace": "ai.dueros.device_interface.screen",
+      "name": "LinkClicked",
       "messageId": "wp1514205733437"
     },
     "payload": {
-      "token": "YXVkaW9fbXVzaWMrZXlKaWIzUmZhV1FpT2lKaGRXUnBiMTl0ZFhOcFl5SXNJbkpsYzNWc2RGOTBiMnRsYmlJNkltRmtOekkzTTJSak9XRTNNalE1WldabVkySmpPR0ZoTUdZell6Y3haV0k0SWl3aVltOTBYM1J2YTJWdUlqb2lNVEF6TURjeU16VXlOaUo5",
-      "accept": [
-        {
-          "namespace": "ai.dueros.device_interface.screen_extended_card",
-          "name": [
-            "RenderPlayerInfo"
-          ]
-        }
-      ]
+      "url": "dueros://audio_unicast/play?list_id=57353474697"
     }
   }
 }
